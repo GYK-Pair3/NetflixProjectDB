75c834973bd1f5454f031f5d2878d1f51817fe3c
FATMA ELİK <fatma_elik@hotmail.com>
2025-04-21 20:29:23 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:fatma@localhost:5432/NetflixProjectDB")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://*******")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
